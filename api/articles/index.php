f48efe7174b361337aec3e496ec21873409fbb69
thejeshgn <i@thejeshgn.com>
2015-12-31 00:48:00 +0530

    changed api get

--- api/articles/index.php
+++ api/articles/index.php
@@ -1,6 +1,15 @@
 <?php
-// Convert RSS feed to JSON, stripping out all but basic HTML
-$output = file_get_contents('https://raw.githubusercontent.com/thejeshgn/what_to_cook/master/data/recipies.json');
+
+function curl($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    $return = curl_exec($ch);
+    curl_close ($ch);
+    return $return;
+}
+
+$output = curl('https://raw.githubusercontent.com/thejeshgn/what_to_cook/master/data/recipies.json'); 
 
 //echo json_encode($output);
 echo $output;
